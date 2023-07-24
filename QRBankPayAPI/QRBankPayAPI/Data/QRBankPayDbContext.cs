35476ba75b456b258d1c0122259b6e5b9bd20baf
Juan Esteban Alvarez Ruiz <70873277+juanes030@users.noreply.github.com>
2023-07-24 14:59:53 -0500

    controlador cliente
    se crea controlador cliente

---
+++ QRBankPayAPI/QRBankPayAPI/Data/QRBankPayDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using QRBankPayAPI.Data.Models;
+
+namespace QRBankPayAPI.Data
+{
+    public class QRBankPayDbContext : DbContext
+    {
+        public QRBankPayDbContext (DbContextOptions<QRBankPayDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<QRBankPayAPI.Data.Models.Client> Client { get; set; } = default!;
+    }
+}
--- QRBankPayAPI/QRBankPayAPI/Program.cs
+++ QRBankPayAPI/QRBankPayAPI/Program.cs
@@ -1,4 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using QRBankPayAPI.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<QRBankPayDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("QRBankPayDbContext") ?? throw new InvalidOperationException("Connection string 'QRBankPayDbContext' not found.")));
 
 // Add services to the container.
 
